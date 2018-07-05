fcdcdaf0cd53635ae5ad4975fd6b1aecbbe8f58d
decaffeinate <hiteshprab@gmail.com>
2018-07-05 16:13:35 +0000

    decaffeinate: Convert client.coffee and 2 other files to JS

--- src/message.js
+++ src/message.js
@@ -1,6 +1,15 @@
-class Message
-    constructor: (data = {}) ->
-        for k of (data or {})
-            @[k] = data[k]
+/*
+ * decaffeinate suggestions:
+ * DS207: Consider shorter variations of null checks
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+class Message {
+    constructor(data) {
+        if (data == null) { data = {}; }
+        for (let k in (data || {})) {
+            this[k] = data[k];
+        }
+    }
+}
 
-module.exports = Message
+module.exports = Message;
