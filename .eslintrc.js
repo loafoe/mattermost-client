7c37a0e6c46acc2b2a72f601c443df4e2df3f22b
fcombes <frederic.combes@i-run.fr>
2020-08-13 18:40:49 +0200

    fixup! feat(build): Add jest for test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,36 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es6: true,
-    node: true,
-  },
-  extends: [
-    'airbnb-base',
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-  },
-  rules: {
-      'indent': [2, 4],
-      'no-underscore-dangle': 'off',
-      'camelcase': 'off',
-      'no-unused-vars': 'off'
-  },
+    env: {
+        browser: true,
+        commonjs: true,
+        es6: true,
+        node: true,
+    },
+    extends: [
+        'airbnb-base',
+    ],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+    },
+    rules: {
+        'indent': [2, 4],
+        'no-underscore-dangle': 'off',
+        'camelcase': 'off',
+        'no-unused-vars': 'off'
+    },
+    overrides: [
+        {
+            files: [
+                "**/*.test.js",
+                "**/*.spec.js",
+                "**/*.spec.jsx"
+            ],
+            env: {
+                jest: true
+            }
+        }
+    ]
 };
